c16897eee057b416952d4a178a4dd3008bbec7d2
Alex <94188621+AlexLaros@users.noreply.github.com>
2021-11-21 13:06:34 +0200

    Update task_3.py

--- lesson_4/task_3.py
+++ lesson_4/task_3.py
@@ -1,1 +1,27 @@
+# Навание программы
+print("Проверка числа на наличие соседних одинаковых цифр")
 
+# Ввод числа
+number = int(input("Введите натуральное число:"))
+num_str = str(number)  # Преоброзование целочисленную переменную в строковую
+
+# Вывод числа
+print(f"Ваше число: {num_str}")
+
+# Проверка "соседей" на равенство
+i = 0
+neighbor_count = 0
+while len(num_str):
+    if i == len(num_str)-1:
+        break
+    else:
+        if num_str[i] == num_str[i+1]:
+            neighbor_count += 1
+    i += 1
+
+# Вывод после проверки
+print("Наличие одинаковых соседей: ", end=" ")
+if neighbor_count > 1:
+    print("Да.")
+else:
+    print("Нет.")
