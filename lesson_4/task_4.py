e61f203e70182866df8fa4ad8f9f5c9c16970613
Alex <94188621+AlexLaros@users.noreply.github.com>
2021-11-21 13:06:57 +0200

    Update task_4.py

--- lesson_4/task_4.py
+++ lesson_4/task_4.py
@@ -1,1 +1,21 @@
+# Ввод слов через пробел
+words = input("Введите два слова через пробел:")
 
+# проверка наличия пробела между слов
+while len(words) > 0:
+    if words.count(' ') == 0:
+        words = input("Попробуйте ещё раз: ")
+        continue
+    else:
+        break
+
+# функция разварота слов
+def reverse():
+    words_list = words.split(" ")
+    str1 = str(words_list[0])
+    str2 = str(words_list[1])
+    print(f"Дано:\n{str1.title() +' '+ str2.title()}")
+    print(f"Вывод: \n{str1[::-1].title() + ' ' + str2[::-1].title()}")
+
+
+reverse()
