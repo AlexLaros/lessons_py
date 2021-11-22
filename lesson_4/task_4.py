497856d95b46fc2ec867ad67ede5c20b640c49b8
Alex <94188621+AlexLaros@users.noreply.github.com>
2021-11-22 13:39:09 +0200

    Update task_4.py

--- lesson_4/task_4.py
+++ lesson_4/task_4.py
@@ -1,21 +1,51 @@
 # Ввод слов через пробел
 words = input("Введите два слова через пробел:")
 
-# проверка наличия пробела между слов
+# функция разваорота
+def reverse():
+    if point == 1:
+        word1 = words_list[0]
+        word2 = words_list[1]
+        print(f"Дано:\n{word1 + ' ' + word2}")
+        print(f"Вывод: \n{word1[::-1].title() + ' ' + word2[::-1].title()}")
+    elif point == 2:
+        word1 = no_spaces_words_list[0]
+        word2 = no_spaces_words_list[1]
+        print(f"Дано:\n{word1 +' '+ word2}")
+        print(f"Вывод: \n{word1[::-1].title() + ' ' + word2[::-1].title()}")
+
+
+# проверка наличия двух слов
 while len(words) > 0:
-    if words.count(' ') == 0:
+    if words.count(" ") == 0:
         words = input("Попробуйте ещё раз: ")
         continue
     else:
-        break
-
-# функция разварота слов
-def reverse():
-    words_list = words.split(" ")
-    str1 = str(words_list[0])
-    str2 = str(words_list[1])
-    print(f"Дано:\n{str1.title() +' '+ str2.title()}")
-    print(f"Вывод: \n{str1[::-1].title() + ' ' + str2[::-1].title()}")
-
-
-reverse()
+        words_list = words.split(" ", maxsplit= -1)
+        if words_list.count("") == 0:
+            if len(words_list) == 2:
+                if len(words_list[0]) == 0 and len(words_list[1]) == 0:
+                    words = input("Попробуйте ещё раз: ")
+                    continue
+                else:
+                    point = 1
+                    reverse()
+                    break
+            else:
+                words = input("Попробуйте ещё раз: ")
+                continue
+        else:
+            no_spaces_words_list = []
+            no_spaces_words_list = " ".join(words_list).split()
+            if len(no_spaces_words_list) == 2:
+                if len(no_spaces_words_list[0]) == 1 and len(no_spaces_words_list[1]) == 1:
+                    words = input("Попробуйте ещё раз: ")
+                    continue
+                else:
+                    point = 2
+                    reverse()
+                    break
+            else:
+                words = input("Попробуйте ещё раз: ")
+                continue
+    break
