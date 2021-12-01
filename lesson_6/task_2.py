ac68f0c0741d6860964c1ff73571dae55e53ad23
Alex <94188621+AlexLaros@users.noreply.github.com>
2021-12-01 19:16:58 +0200

    Update task_2.py

--- lesson_6/task_2.py
+++ lesson_6/task_2.py
@@ -1,1 +1,15 @@
+import random
+size = random.randint(3, 10)
+lst = [random.randint(1, 50) for i in range(size)]
+print(f"Создан список случайного размера от 3 до 10 и\nиз случайного набора чисел от 1 до 50:\n{lst}")
+print("Элементы списка нумеруются начиная с НУЛЯ.")
+k = 1
+c = 6
+lst.append("что-то")
+print(lst)
+lst = lst[:k] + lst[-1:] + lst[k:-1]
+lst[k] = c
+print(lst)
+
+
 
