6a8c4757f34d25e119d44b5c43d60d4ea861dd5f
Alex <94188621+AlexLaros@users.noreply.github.com>
2021-12-05 14:15:43 +0200

    Update task_3.py

--- lesson_6/task_3.py
+++ lesson_6/task_3.py
@@ -5,6 +5,7 @@ lst1 = [random.randint(1, 10) for i in range(5)]
 lst2 = [random.randint(1, 10) for j in range(5)]
 print(f"Даны два списка:\n{lst1}\n{lst2}.")
 lst1 += lst2
-# функция поиска уникальных элементов среди двух списков
-lst3 = [lst1.append(i) for i in range(len(lst1) + 1) if lst1.count(i) == 1]
-print(f"Количество уникальных элементов: {len(lst3)}")
+# функция поиска и вывода уникальных элементов среди двух списков
+print(f"Количество уникальных элементов:\n"
+      f"{len([lst1.append(i) for i in range(len(lst1) + 1) if lst1.count(i) == 1])}"
+      )
