1bf936626d7d193ce0a8bff45d973cc6582af2eb
Alex <94188621+AlexLaros@users.noreply.github.com>
2021-12-14 02:37:45 +0200

    Create task_1.py

---
+++ lesson_9/task_1.py
@@ -0,0 +1,17 @@
+def find_sum_of(lst, a):
+    summa = []
+    for i in lst:
+        for j in lst:
+            if j <= i:
+                continue
+            else:
+                summa.append(i + j)
+    for i in range(len(set(summa))):
+        if a in summa:
+            return True
+        else:
+            return False
+
+
+print(find_sum_of([1, 2, 3, 4], 8))
+print(find_sum_of([1, 22, 11, 2, 14, 12, 10, 3], 33))
