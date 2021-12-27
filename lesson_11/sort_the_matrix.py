a34f446fde6840f80f40f053ec4ad8e7e3b2a400
Alex <94188621+AlexLaros@users.noreply.github.com>
2021-12-27 20:44:30 +0200

    Update sort_the_matrix.py

--- lesson_11/sort_the_matrix.py
+++ lesson_11/sort_the_matrix.py
@@ -2,31 +2,19 @@ import random
 
 # Ввод размера квадратной матрицы
 m = int(input("Введите размер квадратной матрицы больше 5: "))
-# генерация матрицы размера m из случайных чисел
-matrix_lst = [[random.randint(1, 50) for i in range(m)] for j in range(m)]
-
-# сумма элементов по столбцам
-sum_lst = []
-for i in range(m):
-    summa = 0
-    for j in range(m):
-        summa += matrix_lst[j][i]
-    sum_lst.append(summa)
 
 
-def show_result(s):
-    print()
-    print(s)
-    for i in range(m):
-        for j in range(m):
-            print(matrix_lst[i][j], end='  ')
-        print()
-    for i in sum_lst:
-        print(i, end=' ')
-
-
+# Функция сортировки матрицы
 def sort_the_matrix():
-    # сортировка столбцов
+    # сортировка столбоцов по сумме их элементов, по возрастанию
+    for i in range(len(matrix_lst)):
+        for j in range(1, m):
+            if matrix_lst[-1][j - 1] > matrix_lst[-1][j]:
+                for k in range(len(matrix_lst)):
+                    matrix_lst[k][j], matrix_lst[k][j - 1] = matrix_lst[k][j - 1
+                                                                           ], \
+                                                             matrix_lst[k][j]
+        # сортировка  самих столбцов
     for run in range(m - 1):
         for i in range(m):
             for j in range(m - 1):
@@ -43,13 +31,32 @@ def sort_the_matrix():
                                 i], matrix_lst[
                                 j][i]
 
+# функция вывода матрицы
+def show_result(s):
+    print(s)
+    for i in range(len(matrix_lst)):
+        for j in range(m):
+            print("%4d" % matrix_lst[i][j], end='')
+        print()
 
+# проверка значения числа m
 while True:
-    if m <= 5:
-        m = int(input("Ошибка!\n Введите размер больше 5: "))
-        continue
-    else:
+    if m > 5:
+        matrix_lst = [[random.randint(1, 50) for i in range(m)] for j in
+                      range(m)]
+        sum_lst = []
+        matrix_lst.append(sum_lst)
+        # подсчет суммы каждого из столбцов и занесение их в список
+        for i in range(m):
+            summa = 0
+            for j in range(m):
+                summa += matrix_lst[j][i]
+            matrix_lst[-1].append(summa)
         show_result("До сортировки:")
         sort_the_matrix()
         show_result("После сортировки: ")
         break
+    else:
+        print("Ошибка!")
+        m = int(input("Введите размер квадратной матрицы больше 5: "))
+        continue
