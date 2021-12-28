9ca2e43b988ee70f187e9b7f7102678a426b4da2
Alex <94188621+AlexLaros@users.noreply.github.com>
2021-12-29 00:18:00 +0200

    Create main.py

---
+++ lesson_12/main.py
@@ -0,0 +1,7 @@
+import sum_of_columns_rows_of_a_matrix as socroam
+
+m = socroam.create_the_matrix()
+socroam.sum_of_columns(m)
+socroam.sum_of_rows(m)
+socroam.show_the_matrix(m)
+# print(help(socroam))
