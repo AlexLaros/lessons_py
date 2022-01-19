98fb3b80b416c034bebec2de70bf2460b19129e0
Alex <94188621+AlexLaros@users.noreply.github.com>
2022-01-19 15:46:56 +0200

    Update digital_counter.py

--- lesson_16/digital_counter.py
+++ lesson_16/digital_counter.py
@@ -10,39 +10,39 @@ class DigitalCounter:
         self.counter = min
 
     def increase_and_show(self):
-        # Увеличение на 1 только при условии,
+        # Увеличение на 1 тоько при условии,
         # если счетчик не равен max
         if self.counter < self.max:
             self.counter += 1
-            print(f"Counter increased to {self.counter}.")
+            return f"Counter increased to {self.counter}."
         else:
-            print("---Counter value is out of range!---")
+            return "---Counter value is out of range!---"
         pass
 
     def show_counter_value(self):
-        print(f"Counter value:\n{self.counter}")
+        return f"Counter value:\n{self.counter}"
 
 
     def reset_to_min(self):
         self.counter = self.min
-        print("Counter value is minimized.")
+        return "Counter value is minimized."
 
 # Создаем экземпляр класса
 c1 = DigitalCounter(1, 5)
 # смотрим значение
-c1.show_counter_value()
+print(c1.show_counter_value())
 # проверка метода увеличения,
 # чтобы значение счетчика не увеличивалось
 # при достижении максимума
-c1.increase_and_show()
-c1.increase_and_show()
-c1.increase_and_show()
-c1.increase_and_show()
-c1.increase_and_show()
-c1.increase_and_show()
+print(c1.increase_and_show())
+print(c1.increase_and_show())
+print(c1.increase_and_show())
+print(c1.increase_and_show())
+print(c1.increase_and_show())
+print(c1.increase_and_show())
 # Смотрим значение счетчика
-c1.show_counter_value()
+print(c1.show_counter_value())
 # Проверка метода сброса до минимума
 c1.reset_to_min()
 # Смотрим значение счетчика после сброса
-c1.show_counter_value()
+print(c1.show_counter_value())
